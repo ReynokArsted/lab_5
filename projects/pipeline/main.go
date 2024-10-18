19e20d092a859df13db78a3ba836725e216da290
ReynokArsted <varicheva22@gmail.com>
2024-10-19 00:45:02 +0300

    new code

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,51 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+import (
+	"fmt"
+	"strconv"
+)
+
+func removeDuplicates(inputStream chan string, outputStream chan string) {
+	defer close(outputStream)
+	pre := ""
+	for val := range inputStream {
+		if pre != "" {
+			if val != pre {
+				outputStream <- val
+			}
+			pre = val
+		} else {
+			pre = val
+			outputStream <- pre
+		}
+	}
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	fmt.Println("/* Эта программа убирает повторения цифр подряд из введённой последовательнсти цифр */")
+	inChan := make(chan string)
+	outChan := make(chan string)
+	go removeDuplicates(inChan, outChan)
+
+	var numQueue string
+repeat:
+	fmt.Print("Введите последовальность чисел: ")
+	fmt.Scan(&numQueue)
+	if _, err := strconv.Atoi(numQueue); err != nil {
+		fmt.Println("Было введено то, что не является последовательностью чисел. Пожалуйста, повторите ввод!")
+		goto repeat
+	} else {
+		go func() {
+			defer close(inChan)
+			for _, num := range numQueue {
+				inChan <- string(num)
+			}
+		}()
+	}
+
+	fmt.Print("Результат: ")
+	for num := range outChan {
+		fmt.Print(num)
+	}
+	fmt.Println()
 }
